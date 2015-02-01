c393e2873cac1d8a235a34a316f68ac6d10ec1a4
Yuri Kaspervich <ykas.gg@gmail.com>
2015-02-02 00:04:00 +0300

    Add pagination

--- sportsstore/controllers/productListControllers.js
+++ sportsstore/controllers/productListControllers.js
@@ -1,13 +1,33 @@
 angular.module("sportsStore")
-  .controller("productListCtrl", function($scope) {
+  .constant("productListActiveClass", "btn-primary")
+  .constant("productListPageCount", 2)
+  .controller("productListCtrl", function($scope, $filter,
+    productListActiveClass, productListPageCount) {
     var selectedCategory = null;
+    
+    $scope.selectedPage = 1;
+    $scope.pageSize = productListPageCount;
     
     $scope.selectCategory = function(newCategory) {
       selectedCategory = newCategory;
+      $scope.selectedPage = 1;
+    }
+    
+    $scope.selectPage = function(newPage) {
+      $scope.selectedPage = newPage;
     }
     
     $scope.categoryFilterFn = function(product) {
       return selectedCategory == null ||
         product.category == selectedCategory;
+    }
+    
+    $scope.getCategoryClass = function(category) {
+      console.log("getCategoryClass called");
+      return selectedCategory == category ? productListActiveClass : "";
+    };
+    
+    $scope.getPageClass = function(page) {
+      return $scope.selectedPage == page ? productListActiveClass : "";
     }
 });
--- sportsstore/filters/customFilters.js
+++ sportsstore/filters/customFilters.js
@@ -1,4 +1,5 @@
-angular.module("customFilters", []).filter("unique", function() {
+angular.module("customFilters", [])
+.filter("unique", function() {
   return function(data, propertyName) {
     if (angular.isArray(data) && angular.isString(propertyName)) {
       var results = [];
@@ -17,4 +18,34 @@ angular.module("customFilters", []).filter("unique", function() {
       return data;
     }
   }
-});
+})
+.filter("range", function($filter) {
+  return function(data, page, size) {
+    if (angular.isArray(data) && angular.isNumber(page) && angular.isNumber(size)) {
+      var startIndex = (page - 1) * size;
+      
+      if (data.length < startIndex) {
+        return [];
+      } else {
+        return $filter("limitTo")(data.splice(startIndex), size);
+      }
+    } else {
+      return data;
+    }
+  }
+})
+.filter("pageCount", function() {
+  return function(data, size) {
+    if (angular.isArray(data)) {
+      var result = [];
+      
+      for (var i = 0; i < Math.ceil(data.length / size); i++) {
+        result.push(i);
+      }
+      
+      return result;
+    } else {
+      return data;
+    }
+  }
+})
