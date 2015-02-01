48ba59054256b1d2b255b3c2790154281d555bf0
Yuri Kaspervich <ykas.gg@gmail.com>
2015-02-01 03:54:39 +0300

    Move Todo app to separate folder

--- todo/server.js
+++ todo/server.js
@@ -2,5 +2,5 @@ var connect = require("connect"),
     serveStatic = require("serve-static");
 
 var app = connect();
-app.use(serveStatic("../angularjs-examples"));
+app.use(serveStatic("../todo"));
 app.listen(5000)
